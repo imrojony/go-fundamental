f0bf57c6a851a79d764a88cb9348d205926ec2c2
imrojony <imrojony@gmail.com>
2018-05-01 14:29:51 +0600

    few variables added

--- variables.go
+++ variables.go
@@ -6,7 +6,9 @@ import (
 )
 
 var (
-	name, course , module = "rojony","go course", 3.2
+	name ="rojony"
+	course ="go course"
+	module =  3.2
 )
 func main()  {
 	fmt.Println("name is set to ", name , " and is type of ", reflect.TypeOf(name))
