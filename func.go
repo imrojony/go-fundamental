8bd9ac59ebc7f9f5c0d2e9cdeebbfe7e1a04457b
imrojony <imrojony@gmail.com>
2018-05-01 18:20:22 +0600

    vardict function practiced

--- func.go
+++ func.go
@@ -12,8 +12,23 @@ func main() {
 	s1,s2 :=converter(module, author)
 	fmt.Println(s1)
 	fmt.Println(s2)
+	bestFinished := bestLeagueFinishes(10,2,12,23,34)
+	fmt.Println(bestFinished)
 
 }
+func bestLeagueFinishes(finishes ...int)  int{
+	best := finishes[0]
+	//fmt.Println(best)
+	for _, i := range finishes {
+		if i < best {
+			best= i
+		}
+	}
+	return best
+
+}
+
+
 func converter(s1, s2 string) (str1,str2 string) {
 
 	s1=strings.Title(s1)
