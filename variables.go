3b06188ab3215f03c95bff63481b4bc56ae0a96b
imrojony <imrojony@gmail.com>
2018-05-01 14:38:05 +0600

    used local variable

--- variables.go
+++ variables.go
@@ -5,12 +5,17 @@ import (
 	"reflect"
 )
 
-var (
-	name ="rojony"
-	course ="go course"
-	module =  3.2
-)
+//var (
+//	name ="rojony"
+//	course ="go course"
+//	module =  3.2
+//)
 func main()  {
+		name :="rojony"
+		course :="go course"
+		module :=  3.2
+
+	fmt.Println(course)
 	fmt.Println("name is set to ", name , " and is type of ", reflect.TypeOf(name))
 	fmt.Println("module is set to ",module, " and is type of ",reflect.TypeOf(module))
 }
