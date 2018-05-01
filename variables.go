1786cdf0304f91dc27ab833c62e88e076501cc86
imrojony <imrojony@gmail.com>
2018-05-01 15:27:41 +0600

    function passing by value

--- variables.go
+++ variables.go
@@ -12,11 +12,19 @@ import (
 //)
 func main()  {
 		name :="rojony"
-		//course :="go course"
+		course :="go course"
 		module :=  3.2
-		ptr := &module
 
-	fmt.Println(ptr, *ptr)
+
+
 	fmt.Println("name is set to ", name , " and is type of ", reflect.TypeOf(name))
 	fmt.Println("module is set to ",module, " and is type of ",reflect.TypeOf(module))
+
+	fmt.Println(changecousre(course))
+	fmt.Println(course)
+}
+func changecousre(course string)  string{
+	course ="php course"
+	//fmt.Println(course)
+	return course
 }
