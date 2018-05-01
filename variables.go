9abf6feb056b92676a15f6d73cfc1516258ee228
imrojony <imrojony@gmail.com>
2018-05-01 14:52:18 +0600

    used pointer

--- variables.go
+++ variables.go
@@ -12,10 +12,11 @@ import (
 //)
 func main()  {
 		name :="rojony"
-		course :="go course"
+		//course :="go course"
 		module :=  3.2
+		ptr := &module
 
-	fmt.Println(course)
+	fmt.Println(ptr, *ptr)
 	fmt.Println("name is set to ", name , " and is type of ", reflect.TypeOf(name))
 	fmt.Println("module is set to ",module, " and is type of ",reflect.TypeOf(module))
 }
