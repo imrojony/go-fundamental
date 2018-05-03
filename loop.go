b231e4f593503c28232916282184018ebf37ac33
imrojony <imrojony@gmail.com>
2018-05-03 17:34:37 +0600

    sum added with loop

---
+++ loop.go
@@ -0,0 +1,27 @@
+//package main
+
+//import (
+//	"fmt"
+//)
+//
+//func main() {
+//
+//	for i:= 10; i>=0; i-- {
+//		if i== 0{
+//			fmt.Println("boom")
+//
+//		}
+//		fmt.Println(i)
+//	}
+//}
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+}
